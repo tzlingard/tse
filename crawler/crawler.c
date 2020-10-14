c1c7cb1773bbf39e01d0ea7fd0a8dab609f83fcc
attiasa22 <f003mbx@hub4.thayer.dartmouth.edu>
2020-10-14 19:51:12 -0400

    added to crawler.c

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
-
+#include <webpage.h>
 int main(void) {
     printf("hello\n");
-    return 0;
+		int INITDEPTH = 0;
+		webpage_t page = *webpage_new("https://thayer.github.io/engs50/",INITDEPTH , NULL);
+
+		if(webpage_fetch(page)){
+			char *html = webpage_getHTML(page);
+			printf("Found html: %s\n", html);
+
+			int pos = 0;
+			char *result;
+			while ((pos = webpage_getNextURL(page, pos, &result)) > 0) {
+				bool IsInternalURL(char *url);
+				printf("Found url: %s\n", result);
+				free(result);
+			}
+		}
+		else{
+			exit EXIT_FAILURE;
+		}
+
+		/*return 0;*/
 }
