99fcf067181b7b450a6f15fde79065b9fb8cf587
Thomas <thomas.z.lingard.22@dartmouth.edu>
2020-10-15 13:12:41 -0400

    website fetching up and running

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include "../utils/webpage.h"
 
 int main(void) {
-    printf("hello\n");
+    char *html;
+    webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
+    if(webpage_fetch(page)) {
+        html = webpage_getHTML(page);
+        char* result;
+        int i = 0;
+        while ((i = webpage_getNextURL(page, i, &result)) > 0) {
+            if(IsInternalURL(result)) {
+                printf("Internal: %s\n",result);
+            }
+            else {
+                printf("External: %s\n",result);
+            }
+            free(result);
+        }
+    }
+    else {
+        exit(EXIT_FAILURE);
+    }
+    webpage_delete(page);
+    exit(EXIT_SUCCESS);
     return 0;
 }
