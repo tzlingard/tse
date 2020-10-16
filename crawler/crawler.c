df0c180777d48e54416bf3867ab6643a16810ad8
Vann Guarnieri <Francesco.V.Guarnieri.21@dartmouth.edu>
2020-10-16 16:09:41 -0400

    Fixed segfault in codingstyle check

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -4,42 +4,51 @@
 #include <string.h>
 
 int main(void) {
-    char *html;
-    queue_t* internals= qopen();
-    webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
-    if(webpage_fetch(page)) {
-        html = webpage_getHTML(page);
-        char* result;
-        int i = 0;
-        while ((i = webpage_getNextURL(page, i, &result)) > 0) {
-            if(IsInternalURL(result)) {
-                qput(internals, result);
-                printf("Internal: %s\n",result);
-            }
-            else {
-                printf("External: %s\n",result);
-            }
-            free(result);
-        }
-    }
-    else {
-        exit(EXIT_FAILURE);
-    }
-    int i;
-    int styleCount = 0;
-    for(i=1; i<sizeof(internals); ++i){
-        webpage_t* internalPage = (webpage_t*)qget(internals); /*pop URL from queue, and store it*/  
-        printf("URL: %s\n", webpage_getURL(internalPage)); /*print out URL*/
-        
-        char *html= webpage_getHTML(internalPage); /*get the html*/
-        if (strcmp(html, "CodingStyle.html")){ /*if the html is CodingStyle.html*/
-            styleCount=styleCount+1; /*add to our counter*/ 
-        }
-        qclose(internals);
-        printf("There are %d CodingStyle.html entries\n", styleCount);
-        
-    }
-    webpage_delete(page);
-    exit(EXIT_SUCCESS);
-    return 0;
+	char *html = NULL;
+	queue_t* internals= qopen();
+	webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
+	if(webpage_fetch(page)) {
+		html = webpage_getHTML(page);
+		char* result = NULL;
+		int i = 0;
+		while ((i = webpage_getNextURL(page, i, &result)) > 0) {
+			if(IsInternalURL(result)) {
+				qput(internals, result);
+				printf("Internal: %s\n",result);
+			}
+			else {
+				printf("External: %s\n",result);
+			}
+			//free(result);
+			// ^can't free yet or stylecount check wont work
+		}
+	}
+	else {
+		exit(EXIT_FAILURE);
+	}
+	
+	
+	//Ensure that there are 2 stylecounts
+	int i;
+	int styleCount = 0;
+	char* styleURL = "https://thayer.github.io/engs50/Resources/CodingStyle.html";
+	for(i=1; i<sizeof(internals); ++i){
+		char* currURL = (char*)(qget(internals)); /*pop URL from queue, and store it*/  
+		//	printf("URL: %s\n", currURL);
+    
+		
+		if (strcmp(currURL, styleURL)==0){ /*if the html is CodingStyle.html*/
+			styleCount=styleCount+1; /*add to our counter*/ 
+		}
+	}
+	while(qget(internals)!=NULL){
+	}
+	
+	qclose(internals);
+	printf("There are %d CodingStyle.html entries\n", styleCount);
+  
+  
+	webpage_delete(page);
+	exit(EXIT_SUCCESS);
+	return 0;
 }
