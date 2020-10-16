de282e21da2168a72693b9d44a203554bb8934aa
Thomas <thomas.z.lingard.22@dartmouth.edu>
2020-10-16 17:46:23 -0400

    updated to queue of websites

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -7,13 +7,15 @@ int main(void) {
 	char *html = NULL;
 	queue_t* internals= qopen();
 	webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
+	webpage_t *new;
 	if(webpage_fetch(page)) {
 		html = webpage_getHTML(page);
 		char* result = NULL;
 		int i = 0;
 		while ((i = webpage_getNextURL(page, i, &result)) > 0) {
 			if(IsInternalURL(result)) {
-				qput(internals, result);
+				new = webpage_new(result, 1, NULL); //create a new webpage for the given URL
+				qput(internals, new);
 				printf("Internal: %s\n",result);
 			}
 			else {
@@ -31,19 +33,18 @@ int main(void) {
 	//Ensure that there are 2 stylecounts
 	int i;
 	int styleCount = 0;
+	webpage_t* p;
 	char* styleURL = "https://thayer.github.io/engs50/Resources/CodingStyle.html";
-	for(i=1; i<sizeof(internals); ++i){
-		char* currURL = (char*)(qget(internals)); /*pop URL from queue, and store it*/  
-		//	printf("URL: %s\n", currURL);
-    
-		
+	char* currURL;
+	int len = sizeof(internals);
+	for(i=0; i<len-1; i++){
+		p = (webpage_t*)(qget(internals)); /*pop URL from queue, and store it*/  
+		currURL = webpage_getURL(p);
+		printf("URL: %s\n", currURL);
 		if (strcmp(currURL, styleURL)==0){ /*if the html is CodingStyle.html*/
 			styleCount=styleCount+1; /*add to our counter*/ 
 		}
-	}
-	while(qget(internals)!=NULL){
-	}
-	
+	}	
 	qclose(internals);
 	printf("There are %d CodingStyle.html entries\n", styleCount);
   
