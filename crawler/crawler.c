423172fa44be015c3d1876b73e4765ced6924f16
Thomas <thomas.z.lingard.22@dartmouth.edu>
2020-10-18 15:51:18 -0400

    crawler iteration

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include "../utils/webpage.h"
 #include "../utils/queue.h"
+#include "../utils/hash.h"
 #include <string.h>
+#include <stdbool.h>
+
+bool checkURLs(void* url1, const void* url2) {
+	if (strcmp((char*)url1, (char*) url2)) {
+		return false;
+	}
+	else return true;
+}
 
 int main(void) {
 	queue_t* internals= qopen();
+	int len = 0;
+	//hashtable_t* ht = hopen(10);
 	webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
-	webpage_t *new;
 	char* html = NULL;
 	if(webpage_fetch(page)) {
 		html = webpage_getHTML(page);
@@ -14,9 +24,13 @@ int main(void) {
 		int i = 0;
 		while ((i = webpage_getNextURL(page, i, &result)) > 0) {
 			if(IsInternalURL(result)) {
-				new = webpage_new(result, 1, NULL); //create a new webpage for the given URL
-				qput(internals, new);
-				printf("Internal: %s\n",result);
+				//if (hsearch(ht, checkURLs, result, sizeof(result)) == NULL) { //if the URL isn't already in the hash table
+					webpage_t* new = webpage_new(result, 1, NULL); //create a new webpage for the given URL
+					qput(internals, (void*)new);
+					len++;
+					//hput(ht, (void*)result, (char*)&i, sizeof(i));
+					printf("Internal: %s\n",result);
+				//}
 			}
 			else {
 				printf("External: %s\n",result);
@@ -32,7 +46,6 @@ int main(void) {
 	int styleCount = 0;
 	char* styleURL = "https://thayer.github.io/engs50/Resources/CodingStyle.html";
 	char* currURL;
-	int len = sizeof(internals);
 	for(i=0; i<len-1; i++){
 		webpage_t* p = (webpage_t*)(qget(internals)); /*pop URL from queue, and store it*/  
 		currURL = webpage_getURL(p);
@@ -44,6 +57,7 @@ int main(void) {
 	}
 	webpage_delete(page);	
 	qclose(internals);
+	//hclose(ht);
 	printf("There are %d CodingStyle.html entries\n", styleCount);
   
 	exit(EXIT_SUCCESS);
