4660f743781ddefdc94ec44ce3d355bbc8531f1a
Thomas <thomas.z.lingard.22@dartmouth.edu>
2020-10-23 12:45:39 -0400

    indexer

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -4,10 +4,11 @@
 #include "../utils/webpage.h"
 int main(void) {
   webpage_t* top = pageload(1, "../pages");
-  char** word;
-  webpage_getNextWord(top, 0, word);
-  printf("%s", *word);
-  free(word[0]);
+  char* word = (char*)malloc(sizeof(char) * 20);
+  while (webpage_getNextWord(top, 0, &word) != -1) {
+    printf("%s", word);
+  }
+  free(word);
   webpage_delete(top);
   return 0;
 }
