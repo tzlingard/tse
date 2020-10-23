d75adf14746e9fda14a5b2688ed643bb53174c8c
Vann Guarnieri <Francesco.V.Guarnieri.21@dartmouth.edu>
2020-10-23 15:11:00 -0400

    finished step 2

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
-
+#include <ctype.h>
 #include "../utils/pageio.h"
 #include "../utils/webpage.h"
+
+
+void normalizeWord(char* word){
+	int count = 0;
+	while(word[count] != 0){
+		char c = tolower(word[count]);
+		word[count] = c;
+		count++;
+	}
+}
+
 int main(void) {
+	//char* test = malloc(5);
+	//strcpy(test, "TeSt");
+	//normalizeWord(test);
+	//printf("normalized word = %s\n",test);
+	
   webpage_t* top = pageload(1, "../pages");
 	printf("%s\n",webpage_getURL(top));
 	char* word;
 	int pos = 0;
 	while((pos = webpage_getNextWord(top,pos, &word))>0){
-		printf("%s ", word);
-		//		printf("pos = %d\n",pos);
+		normalizeWord(word);
+		printf("%s\n", word);
 		free(word);		
 	}
 
+
   webpage_delete(top);
+	
   return 0;
 }
