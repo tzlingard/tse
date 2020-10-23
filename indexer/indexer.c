59137d17c63ac88040c9602f0b6f425c8a6102c1
Vann Guarnieri <Francesco.V.Guarnieri.21@dartmouth.edu>
2020-10-23 13:23:30 -0400

    fixed indexer

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -5,13 +5,13 @@
 int main(void) {
   webpage_t* top = pageload(1, "../pages");
 	printf("%s\n",webpage_getURL(top));
-	char* word = (char*)malloc(sizeof(char*));
+	char* word;
 	int pos = 0;
 	while((pos = webpage_getNextWord(top,pos, &word))>0){
 		printf("%s ", word);
 		//		printf("pos = %d\n",pos);
 		free(word);		
-		}
+	}
 
   webpage_delete(top);
   return 0;
