f6a845c1c8f8c439a9ffe97c58c4ea92809afca2
Vann Guarnieri <Francesco.V.Guarnieri.21@dartmouth.edu>
2020-10-24 11:23:17 -0400

    fixed normalizeword function

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -26,19 +26,36 @@ void normalizeWord(char* word) {
       char c = tolower(word[count]);  // make the character lowercase
       word[count] = c;
       count++;
-    } else {  // delete non-alphabetic characters
-      word = "";
+    } else {  // delete non-alphabetic words
+      *word = 0;
 			break;
     }
   }
   if (count < 3) {
-    word = "";
+    *word = 0;
+		//printf("count was less than 3, word should be empty. Word = %s\n", word);
   }
 
 }
 
 int main(void) {
-  webpage_t* top = pageload(1, "../pages");
+	printf("non alphabetic words test:\n\n");
+
+	char* alpha = (char*)malloc(10); 
+	strcpy(alpha, "AbCdEf");
+	printf("Normalized %s = ", alpha);
+	normalizeWord(alpha);
+	printf("%s\n", alpha);
+	free(alpha);
+	
+	char* nonalpha = (char*)malloc(10); 
+	strcpy(nonalpha, "aBc123");
+	printf("Normalized %s = ", nonalpha);
+	normalizeWord(nonalpha);
+	printf("%s\n\n", nonalpha);
+	free(nonalpha);
+	
+	webpage_t* top = pageload(1, "../pages");
   hashtable_t* index = hopen(20);
   printf("%s\n", webpage_getURL(top));
   char* word;
@@ -46,17 +63,17 @@ int main(void) {
   while ((pos = webpage_getNextWord(top, pos, &word)) >
          0) {             // loop over each word in the page
     normalizeWord(word);  // convert to alphabetic and lowercase
-    if (word != NULL) {
+    if (strlen(word)>0) {
       printf("%s\n", word);
-      word_t* w;
-      if ((w = (word_t*)hsearch(index, wordUsed, word, strlen(word))) != NULL) {
-        // if word has already been used and placed in the index
+			word_t* w;
+			if ((w = (word_t*)hsearch(index, wordUsed, word, strlen(word))) != NULL) {
+				//   if word has already been used and placed in the index
         (w->freq)++;
-      } else {  // if the word hasn't been used, add it with frequency of 1
-        w->freq = 1;
-        hput(index, (void*)w, w->word, strlen(w->word));
-      }
-    }
+			} else {  // if the word hasn't been used, add it with frequency of 1
+				w->freq = 1;
+				hput(index, (void*)w, w->word, strlen(w->word));
+			}
+		}
     free(word);
   }
   hclose(index);
