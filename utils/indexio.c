dce52388a844e43ca247c6750fd4f627589d08ec
Thomas <thomas.z.lingard.22@dartmouth.edu>
2020-10-28 22:13:31 -0400

    working on step 7

--- utils/indexio.c
+++ utils/indexio.c
@@ -1,13 +1,14 @@
+#include "indexio.h"
+
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "hash.h"
 #include "queue.h"
 #include "webpage.h"
-#include "indexio.h"
 
-FILE *fp_global;
+FILE* fp_global;
 
 typedef struct {  // structure to hold each word of a webpage and its frequency
   char* word;
@@ -22,73 +23,67 @@ typedef struct {
   int freq;
 } docs_t;
 
-
-
-//print to fp the current doc id and the frequency
-void printDocToFile(void *d){
-	docs_t* doc = (docs_t*) d;
-	fprintf(fp_global, "%d %d ", doc->id, doc->freq);
+// print to fp the current doc id and the frequency
+void printDocToFile(void* d) {
+  docs_t* doc = (docs_t*)d;
+  fprintf(fp_global, "%d %d ", doc->id, doc->freq);
 }
 
-//print to fp the word, docs and freqs, 
-void printWordToFile(void* w){
-	word_t* word = (word_t*) w;
-	fprintf(fp_global, "%s ", word->word);
-	qapply(word->freq, printDocToFile);
-	fprintf(fp_global, "\n");
+// print to fp the word, docs and freqs,
+void printWordToFile(void* w) {
+  word_t* word = (word_t*)w;
+  fprintf(fp_global, "%s ", word->word);
+  qapply(word->freq, printDocToFile);
+  fprintf(fp_global, "\n");
 }
 
 /* Save an index to a file*/
 void indexsave(hashtable_t* htp, char* dirname, char* indexnm) {
-	char dest[80];
-	sprintf(dest, "%s/%s", dirname, indexnm);
-	if (access(dest, F_OK)) {
-		fp_global = fopen(dest, "w");
-		happly(htp, printWordToFile);
-		fclose(fp_global);
-	}
-
+  char dest[80];
+  sprintf(dest, "%s/%s", dirname, indexnm);
+  if (access(dest, F_OK) == 0) {
+    fp_global = fopen(dest, "w");
+    happly(htp, printWordToFile);
+    fclose(fp_global);
+  }
 }
 /*
 hashtable_t* indexload(char* dirname, char* indexnm) {
-	return NULL;
+        return NULL;
 }
 */
 
 // Load an index from a file
 hashtable_t* indexload(char* dirname, char* indexnm) {
-	printf("starting\n");
+  printf("starting\n");
   char loc[80];
   sprintf(loc, "%s/%s", dirname, indexnm);
   if (access(loc, R_OK) == 0) {
-
     FILE* fp;
     fp = fopen(loc, "r");
 
     char nextWord[80];
     hashtable_t* index = hopen(20);
 
-		while (fscanf(fp, "%s", nextWord) == 1) {
-
-			word_t* w = (word_t*)malloc(sizeof(word_t*));
-			w -> word = (char*)malloc(strlen(nextWord) * sizeof(char) + 1);
-			strcpy(w->word, nextWord);
-			w->freq = qopen();
+    while (fscanf(fp, "%s", nextWord) == 1) {
+      word_t* w = (word_t*)malloc(sizeof(word_t*));
+      w->word = (char*)malloc(strlen(nextWord) * sizeof(char) + 1);
+      strcpy(w->word, nextWord);
+      w->freq = qopen();
       int docId, count;
-			
+
       while (fscanf(fp, "%d %d", &docId, &count) == 2) {
-				docs_t* d = (docs_t*)malloc(sizeof(docs_t*));
-				d->id = docId;
-				d->freq = count;
-				qput(w->freq, d);
+        docs_t* d = (docs_t*)malloc(sizeof(docs_t*));
+        d->id = docId;
+        d->freq = count;
+        qput(w->freq, d);
       }
-			
-			hput(index, w, w->word, strlen(w->word));
+
+      hput(index, w, w->word, strlen(w->word));
       if (feof(fp)) break;
     }
-		fclose(fp);
+    fclose(fp);
     return (index);
   } else
     return NULL;
 }
-
