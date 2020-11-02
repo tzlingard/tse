270580339dc4fa3ad27affc17c252c7be1cf73c2
Thomas <thomas.z.lingard.22@dartmouth.edu>
2020-11-02 14:58:48 -0500

    worked on step 2

--- querier/query.c
+++ querier/query.c
@@ -45,45 +45,56 @@ int main(int argc, char *argv[]) {
 	char* currchar;
 	char  word[20];
 	bool cont = true;
-
-	while(true){
+	while(true) {
 		input = (char*)malloc(sizeof(char*)*101);
-
 		printf("> ");
 		fgets(input, 100, stdin);
-
 		//break if the input is CTRL D
 		if((int)(*input)==0){
 			free(input);
 			printf("\n");
 			break;
 		}
-		
 		//skip the loop if the input has non numeric characters
 		cont = true;
 		if(!isValid(input)){
 			printf("[invalid query]");
 			cont = false;
 		}
-		
-		currchar=input;//
-		while(cont){
+		currchar = input;//
+		int occurences[34]; //33 is the max number of 3+ letter words in 100 chars
+		int i = 0;
+		while(cont) {
 			//skip past spaces and tabs, if any
-			while( ((int)(*currchar))==9 || ((int)(*currchar))==32){	
+			while( ((int)(*currchar))==9 || ((int)(*currchar))==32) {	
 				currchar+=sizeof(char);
 			}
 			//print current word
-			if(sscanf(currchar, "%s", word)==1){
+			if(sscanf(currchar, "%s", word)==1) {
 				currchar+=strlen(word);
 				normalizeWord(word);
-				printf("%s ", word);
+				if(strcmp(word, "and") != 0 && strcmp(word, "or") != 0 && strlen(word) >= 3) {
+					//skip the reserved words "and" and "or" and words less than 3 letters long
+					
+					printf("%s:%d ", word, occurrence); //TODO: occurrence = first num in index after the given word
+					occurences[i] = occurence;
+					i++;
+				}
 			}
-			else{
+			else {
 				cont=false;
 			}
 		}
+		int rank = 10000;
+		int i;
+		for (i=0; i<34; i++) {
+			//find the minimum number of occurrences and set this as the rank
+			if (occurences[i] < rank) {
+				rank = occurences[i];
+			}
+		}
+		printf("-- %d\n", rank);
 		free(input);
-		printf("\n");
 	}
 	return 0;
 }
