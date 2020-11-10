231cfa4fd586f02c4f5fed1668b40b8716477a6a
Thomas <thomas.z.lingard.22@dartmouth.edu>
2020-11-09 23:38:46 -0500

    wrote lqueue

---
+++ utils/lqueue.c
@@ -0,0 +1,53 @@
+#include "lqueue.h"
+
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "queue.h"
+
+pthread_mutex_t m;
+
+lqueue_t* lqopen() {
+  pthread_mutex_init(&m, NULL);
+  pthread_mutex_lock(&m);
+  queue_t* q = qopen();
+  pthread_mutex_unlock(&m);
+  return ((lqueue_t*)q);
+}
+
+void lqclose(lqueue_t* qp) {
+  pthread_mutex_lock(&m);
+  qclose((queue_t*)qp);
+  pthread_mutex_unlock(&m);
+  pthread_mutex_destroy(&m);
+}
+
+int32_t lqput(lqueue_t* qp, void* elementp) {
+  pthread_mutex_lock(&m);
+  int v = qput((queue_t*)qp, elementp);
+  pthread_mutex_unlock(&m);
+  return (v);
+}
+
+void* lqget(lqueue_t* qp) {
+  pthread_mutex_lock(&m);
+  void* elementp = qget(qp);
+  pthread_mutex_unlock(&m);
+  return (elementp);
+}
+
+void lqapply(lqueue_t* qp, void (*fn)(void* elementp)) {
+  pthread_mutex_lock(&m);
+  qapply(qp, fn);
+  pthread_mutex_unlock(&m);
+}
+
+void* lqsearch(lqueue_t* qp, bool (*searchfn)(void* elementp, const void* keyp),
+               const void* skeyp) {
+  pthread_mutex_lock(&m);
+  void* returnp = qsearch(qp, searchfn, skeyp);
+  pthread_mutex_unlock(&m);
+  return (returnp);
+}
---
+++ utils/lqueue.h
@@ -0,0 +1,39 @@
+#pragma once
+/*
+ * lqueue.h -- public interface to the locked queue module
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* the queue representation is hidden from users of the module */
+typedef void lqueue_t;
+
+/* create an empty queue */
+lqueue_t* lqopen(void);
+
+/* deallocate a queue, frees everything in it */
+void lqclose(lqueue_t* qp);
+
+/* put element at the end of the queue
+ * returns 0 is successful; nonzero otherwise
+ */
+int32_t lqput(lqueue_t* qp, void* elementp);
+
+/* get the first first element from queue, removing it from the queue */
+void* lqget(lqueue_t* qp);
+
+/* apply a function to every element of the queue */
+void lqapply(lqueue_t* qp, void (*fn)(void* elementp));
+
+/* search a queue using a supplied boolean function
+ * skeyp -- a key to search for
+ * searchfn -- a function applied to every element of the queue
+ *          -- elementp - a pointer to an element
+ *          -- keyp - the key being searched for (i.e. will be
+ *             set to skey at each step of the search
+ *          -- returns TRUE or FALSE as defined in bool.h
+ * returns a pointer to an element, or NULL if not found
+ */
+void* lqsearch(lqueue_t* qp, bool (*searchfn)(void* elementp, const void* keyp),
+               const void* skeyp);
